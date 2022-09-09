bc14a07e1376551a9e9899d8ea96920ec7a049fe
eertt <gemechuabel2022@gmail.com>
2022-09-09 01:31:51 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+/**
+  * main - printing
+  *
+  * Return: Always 0 (Successful)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
